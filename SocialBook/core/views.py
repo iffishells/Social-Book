13eea3dbb4b1871a1db9ee88416218f43954e7f5
iffishells <iffishells@gmail.com>
2022-09-07 11:54:46 +0500

    Signup Module is completed

--- SocialBook/core/views.py
+++ SocialBook/core/views.py
@@ -1,15 +1,18 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 
 from .models import Profile
-from django.contrib.auth.models import User,auth
+from django.contrib.auth.models import User, auth
 from django.contrib import messages
+
+
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
+
 def sign_up(request):
-    if request.method =='POST':
+    if request.method == 'POST':
 
         username = request.POST['Username']
         email = request.POST['email']
@@ -21,11 +24,26 @@ def sign_up(request):
         # if User.objects.filter(email=email):
 
         if password1 == password2:
-            pass
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Already Taken')
+                return redirect('sign_up')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'UserName Already Taken')
+                return redirect('sign_up')
+            else:
+                user = User.objects.create_user(email=email,
+                                                username=username,
+                                                password=password1)
+                user.save()
+                # lets add in current model for the profile model
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model,
+                                                         id_user =user_model.id)
+                new_profile.save()
+                return redirect('sign_up')
         else:
-            messages.info(request,'Passwords are not match')
-            return redirect('signup')
-
+            messages.info(request, 'Passwords are not match')
+            return redirect('sign_up')
 
     else:
         return render(request, 'signup.html')
